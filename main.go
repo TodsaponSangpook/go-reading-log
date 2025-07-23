7f65f4aaed7e7d990e3ec458e6df54aa2146e671
Todsapon (Arm) Sangpook <todsapon.san@truedigital.com>
2025-07-23 20:28:58 +0700

    get config by env

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/todsapon/go-reading-log/config"
 	"github.com/todsapon/go-reading-log/db"
 )
 
 func main() {
+	config.LoadEnv()
 	db.Connect()
-	defer db.Pool.Close()
+	defer db.DB.Close()
 
 	app := fiber.New()
 
-	app.Listen(":8080")
+	app.Listen(config.GetPort())
 }
