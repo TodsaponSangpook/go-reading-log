3dc9891cf606e6de328dd58406d0d455c176a28a
Todsapon (Arm) Sangpook <todsapon.san@truedigital.com>
2025-07-31 20:59:27 +0700

    refactor response data in register and login

---
+++ model/api-response.go
@@ -0,0 +1,27 @@
+package model
+
+import "github.com/gofiber/fiber/v2"
+
+type APIResponse[T any] struct {
+	Success bool   `json:"success"`
+	Message string `json:"message,omitempty"`
+	Data    T      `json:"data,omitempty"`
+}
+
+func SuccessResponse[T any](c *fiber.Ctx, status int, message string, data T) error {
+	res := APIResponse[T]{
+		Success: true,
+		Message: message,
+		Data:    data,
+	}
+	return c.Status(status).JSON(res)
+}
+
+func FailedResponse(c *fiber.Ctx, status int, message string) error {
+	res := APIResponse[any]{
+		Success: false,
+		Message: message,
+		Data:    nil,
+	}
+	return c.Status(status).JSON(res)
+}
