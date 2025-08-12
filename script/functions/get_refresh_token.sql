7b4e98080cd9970feca7abba8a7d15db55327761
Todsapon (Arm) Sangpook <todsapon.san@truedigital.com>
2025-08-12 20:39:20 +0700

    add get_refresh_token.sql

---
+++ script/functions/get_refresh_token.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION public.get_refresh_token(p_user_id integer, p_token character varying)
+ RETURNS TABLE(expires_at timestamp without time zone)
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+	RETURN QUERY
+	SELECT rt.expires_at
+	FROM refresh_token rt
+	WHERE rt.user_id = p_user_id and rt.token = p_token;
+END;
+$function$
+;
