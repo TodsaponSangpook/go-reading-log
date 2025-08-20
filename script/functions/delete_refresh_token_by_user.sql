3f58b8de49ff6fdf34d9d53984e129c6717d82df
Todsapon (Arm) Sangpook <todsapon.san@truedigital.com>
2025-08-20 21:01:32 +0700

    update script

--- script/functions/delete_refresh_token_by_user.sql
+++ script/functions/delete_refresh_token_by_user.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE PROCEDURE public.delete_refresh_token_by_user()
-	LANGUAGE plpgsql
+CREATE OR REPLACE PROCEDURE public.delete_refresh_token_by_user(IN p_user_id integer)
+ LANGUAGE plpgsql
 AS $procedure$
 BEGIN
 	DELETE FROM refresh_token
 	WHERE user_id = p_user_id;
 END;
-$procedure$;
+$procedure$
 ;
--- script/functions/refresh_token_by_user.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE PROCEDURE public.delete_refresh_token_by_user()
-	LANGUAGE plpgsql
-AS $procedure$
-BEGIN
-	DELETE FROM refresh_token
-	WHERE user_id = p_user_id;
-END;
-$procedure$;
-;
